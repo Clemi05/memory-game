123a782f2eabb5380b2fb41309c121c99a8e71ca
Clement Azalbert <clementazalbert04@gmail.com>
2022-04-22 12:38:26 +0200

    added funct checkMatch

--- app.js
+++ app.js
@@ -53,6 +53,9 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random());
 
 const gridDisplay = document.querySelector('#grid');
+let cardsChosen = [];
+let cardsChosenIds = [];
+const cardsMatch = [];
 
 function createBoard() {
   for (let index = 0; index < cardArray.length; index++) {
@@ -66,7 +69,30 @@ function createBoard() {
 
 createBoard();
 
+function checkMatch() {
+  const cards = document.querySelectorAll('img');
+  console.log(cards);
+  console.log('check for a match');
+  if (cardsChosen[0] == cardsChosen[1]) {
+    alert('You found a match!')
+    cards[cardsChosenIds[0]].setAttribute('src', 'images/white.png')
+    cards[cardsChosenIds[1]].setAttribute('src', 'images/white.png')
+    cards[cardsChosenIds[0]].removeEventListener('click', flipCard)
+    cards[cardsChosenIds[1]].removeEventListener('click', flipCard)
+    cardsMatch.push(cardsChosen);
+  }
+  cardsChosen = [];
+  cardsChosenIds = [];
+}
+
 function flipCard() {
   const cardId = this.getAttribute('data-id');
-  console.log('clicked', cardId)
+  cardsChosen.push(cardArray[cardId].name);
+  cardsChosenIds.push(cardId);
+  console.log(cardsChosen);
+  console.log(cardsChosenIds);
+  this.setAttribute('src', cardArray[cardId].img)
+  if(cardsChosen.length === 2) {
+    setTimeout(checkMatch, 500);
+  }
 }
